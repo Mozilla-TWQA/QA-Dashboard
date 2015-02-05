67fffad07abd9846788c8c4e2e28593408e5adee
alison-shiue <alison@alison-u-pc.(none)>
2015-02-05 11:08:29 +0800

    update owners

--- ffos/b2g/owners.js
+++ ffos/b2g/owners.js
@@ -4,88 +4,45 @@
 //Please keep the owner names consistent with phonebook names
 
 var OWNERS = {
-  // "AudioChannel": "Eric Chou", // Firefox OS
-  // "BetaTriage": "?", // Firefox OS
   "Storage": "Alison Shiue",
   "MTP/UMS": "Alison Shiue",
   "Bluetooth": "Teri Wen", // Firefox OS
   "Bluetooth PTS": "Eric Chang, Teri Wen",
   "Text Selection": "*",
-  // "Build Config": "Dylan Oliver", // Core
-  // "Canvas: 2D": "Milan Sreckovic (Benoit Jacob, Jeff Muizelaar)",
-  // "Canvas: WebGL": "Milan Sreckovic (Benoit Jacob, Jeff Gilbert)",
-  // "CSS Parsing and Computation": "Jet Villegas",
-  // "Emulator": "?", // Firefox OS
-  // "FxA": "?", // Firefox OS
-  // "DOM: Apps": "Fabrice Desré",
-  // "DOM: Core & HTML": "Andrew Overholt (Johnny Stenback)", // DOM
-  // "DOM": "Andrew Overholt (2nd: Johnny Stenback)", // DOM
-  // "DOM: Contacts": "David Scravaglieri (Anthony Ricaud)", // DOM
-  // "DOM: Device Interfaces": "Andrew Overholt (Johnny Stenback)", // DOM
-  // "DOM: Events": "Andrew Overholt (Johnny Stenback)",
-  "Gaia": "",
   "Gaia::Bluetooth File Transfer": "Teri Wen",
   "Gaia::Browser": "",
-  "Gaia::Build": "",
   "Gaia::Calendar": "Edward Chen",
   "Gaia::Camera": "",
   "Gaia::Clock": "Edward Chen",
   "Gaia::Contacts": "Johan Lorenzo",
-  "Gaia::Cost Control": "",
+  "Gaia::Cost Control": "Shing Lyu",
   "Gaia::Dialer": "Johan Lorenzo",
   "Gaia::E-Mail": "Edward Chen",
   "Gaia::Everything.me": "",
   "Gaia::First Time Experience": "",
   "Gaia::FMRadio": "",
   "Gaia::Gallery": "",
-  // "Gaia::GithubBot": "?",ven
-  "Gaia::Homescreen": "",
+  "Gaia::Homescreen": "Hermes Cheng",
   "Gaia::Keyboard": "Gerry Chang",
   "Gaia::Music": "",
   "Gaia::Notes": "",
-  // "Gaia::PDF Viewer": "?",
-  // "Gaia::PerformanceTest": "?",
   "Gaia::Ringtones": "Alison Shiue",
   "Gaia::Search": "",
   "Gaia::Settings": "*",
   "Gaia::SMS": "Eric Chang",
   "Gaia::System": "", //  Tim relies on Gregor to actively throwing bugs over.
   "Gaia::System::Browser Chrome": "",
-  "Gaia::System::Input Mgmt": "",
   "Gaia::System::Lockscreen": "Gerry Chang",
   "Gaia::System::Window Mgmt": "",
-  // "Gaia::TestAgent": "?",
-  // "Gaia::UI Tests": "?",
   "Gaia::Video": "",
-  // "Gaia::Wallpaper": "?",
-  "Gaia::Wappush": "",
-  // "General": "?", // Firefox OS
-  "GonkIntegration": "", // Firefox OS
-  "General Automation": "", // Release Engineering
   "Geolocation": "", // Core
   "Graphics": "", // Core
-  "Graphics: Layers": "", // Core
-  // "Hardware": "?", // Firefox OS
-  "Hardware Abstraction Layer (HAL)": "", // Core
-  "ImageLib": "",
-  "IPC": "",
-  "JavaScript Engine": "", // Core
-  "JavaScript: GC": "", // Core
-  "Layout": "", // Core
   "MFBT": "",
   "Networking": "Gerry Chang", // Core
   "NFC": "Alison Shiue", // Firefox OS
-  "Panning and Zooming": "", // Core
-  "Performance": "", // Firefox OS
-  "Runtime": "", // Firefox OS
-  // "Simulator": "?", // Firefox OS
   "RIL": "Eric Chang", // Firefox OS
-  "Video/Audio": "", // Core
-  "Video/Audio: Recording": "", // Core
-  "Vendcom": "", // Firefox OS
-  "Widget: Gonk": "",
   "Wifi": "Gerry Chang", // Firefox OS
-  // "WMF": "?" // Firefox OS
   "SystemPlatform": "Gerry Chang",
-  "OTA": "Gerry Chang"
+  "OTA": "Gerry Chang",
+  "Privacy Panel": "Gerry Chang"
 };
