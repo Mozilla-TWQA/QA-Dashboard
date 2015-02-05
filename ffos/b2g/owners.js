672b988851d5656a247bab1761a10e0b2c38571d
alison-shiue <alison@alison-u-pc.(none)>
2015-02-05 13:32:28 +0800

    update owners

--- ffos/b2g/owners.js
+++ ffos/b2g/owners.js
@@ -4,45 +4,36 @@
 //Please keep the owner names consistent with phonebook names
 
 var OWNERS = {
-  "Storage": "Alison Shiue",
-  "MTP/UMS": "Alison Shiue",
   "Bluetooth": "Teri Wen", // Firefox OS
   "Bluetooth PTS": "Eric Chang, Teri Wen",
-  "Text Selection": "*",
-  "Gaia::Bluetooth File Transfer": "Teri Wen",
-  "Gaia::Browser": "",
   "Gaia::Calendar": "Edward Chen",
   "Gaia::Camera": "",
   "Gaia::Clock": "Edward Chen",
-  "Gaia::Contacts": "Johan Lorenzo",
+  "Gaia::Contacts": "Eric Chang",
   "Gaia::Cost Control": "Shing Lyu",
-  "Gaia::Dialer": "Johan Lorenzo",
+  "Gaia::Dialer": "Eric Chang",
   "Gaia::E-Mail": "Edward Chen",
-  "Gaia::Everything.me": "",
-  "Gaia::First Time Experience": "",
+  "Gaia::FTU": "",
   "Gaia::FMRadio": "",
   "Gaia::Gallery": "",
   "Gaia::Homescreen": "Hermes Cheng",
   "Gaia::Keyboard": "Gerry Chang",
   "Gaia::Music": "",
-  "Gaia::Notes": "",
   "Gaia::Ringtones": "Alison Shiue",
   "Gaia::Search": "",
-  "Gaia::Settings": "*",
+  "Gaia::Settings": "",
   "Gaia::SMS": "Eric Chang",
-  "Gaia::System": "", //  Tim relies on Gregor to actively throwing bugs over.
-  "Gaia::System::Browser Chrome": "",
+  "Gaia::System": "",
   "Gaia::System::Lockscreen": "Gerry Chang",
   "Gaia::System::Window Mgmt": "",
   "Gaia::Video": "",
-  "Geolocation": "", // Core
-  "Graphics": "", // Core
-  "MFBT": "",
-  "Networking": "Gerry Chang", // Core
+  "MTP/UMS": "Alison Shiue",
   "NFC": "Alison Shiue", // Firefox OS
-  "RIL": "Eric Chang", // Firefox OS
-  "Wifi": "Gerry Chang", // Firefox OS
-  "SystemPlatform": "Gerry Chang",
   "OTA": "Gerry Chang",
-  "Privacy Panel": "Gerry Chang"
+  "Privacy Panel": "Gerry Chang",
+  "RIL": "Eric Chang", // Firefox OS
+  "Storage": "Alison Shiue",
+  "SystemPlatform": "Gerry Chang",
+  "Text Selection": "*",
+  "Wifi": "Gerry Chang" // Firefox OS
 };
